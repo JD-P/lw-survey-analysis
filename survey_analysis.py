878aceac690cfde832fa1d30587f854518940fd4
John David Pressman <jdpostage@linuxmail.org>
2016-03-30 20:56:37 -0700

    Successfully outputs stripped data.

--- survey_analysis.py
+++ survey_analysis.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import argparse
 import time
@@ -15,6 +16,18 @@ except:
     print("Brigade checking module not included with your distribution, the "
           "--brigade option will fail if used.")
 timegm = calendar.timegm
+
+def sample_results(results, size):
+    """Sample [size] rows from the [results]."""
+    sample = []
+    generator = random.SystemRandom()
+    random_selection = set()
+    while len(random_selection) < size:
+        choice = generator.randrange(0, len(results))
+        random_selection.add(choice)
+    for index in random_selection:
+        sample.append(results[index])
+    return sample
 
 def tally_answers(results, fieldnames):
     """Tally the answers to questions in results."""
@@ -86,18 +99,40 @@ def import_survey_structure(filepath):
 def binary_search_dates(unix_timestamps, date_indexes):
     """Determine the index of the results for each unix_timestamp beyond which
     all further values will be greater."""
-    
+    unix_timestamps.sort()
+    date_indexes.sort()
+    indexes = []
+    if len(unix_timestamps) > 1:
+        mid = round(len(unix_timestamps) / 2)
+        return (binary_search_dates(unix_timestamps[:mid], date_indexes) +
+                binary_search_dates(unix_timestamps[mid:], date_indexes))
+    elif len(unix_timestamps) == 1:
+        return bsearch_basecase(unix_timestamps, date_indexes)
+
+def bsearch_basecase(unix_timestamps, date_indexes):
+    mid = round(len(date_indexes) / 2)
+    if len(date_indexes) == 1:
+        return [date_indexes[0][0]]
+    elif unix_timestamps[0] == date_indexes[mid][1]:
+        return [date_indexes[mid][0]]
+    elif unix_timestamps[0] > date_indexes[mid][1]:
+        bsearch_basecase(unix_timestamps, date_indexes[mid:])
+    else:
+        bsearch_basecase(unix_timestamps, date_indexes[:mid])
+    return "???"
 
 def generate_x_y_count_rise(results, question_answer):
     """Generate the x and y vectors for the count rise plot given a set of
     [results]."""
     date_indexes = []
     for row in results:
-        date_indexes.append((row["id"], row["startdate"]))
+        date_indexes.append((int(row["id"]),
+                             timegm(time.strptime(row["startdate"],
+                                                  "%Y-%m-%d %H:%M:%S"))))
     date_indexes.sort()
     unix_timestamps = []
-    start_timestamp = timegm(time.strptime(date_indexes[0], "%Y-%m-%d %H:%M:%S"))
-    end_timestamp = timegm(time.strptime(date_indexes[-1], "%Y-%m-%d %H:%M:%S"))
+    start_timestamp = date_indexes[0][1]
+    end_timestamp = date_indexes[-1][1]
     six_hours_seconds = 6 * 60 * 60
     while start_timestamp < end_timestamp:
         unix_timestamps.append(start_timestamp)
@@ -105,15 +140,104 @@ def generate_x_y_count_rise(results, question_answer):
     indexes = binary_search_dates(unix_timestamps, date_indexes)
     counts = []
     (question, answer) = question_answer.split("|")
+    results.sort(key=lambda item:item["id"])
+    print(indexes)
     for index in indexes:
         counts.append(tally_answers(results[:index])[question][answer])
     return (list(range(1, len(unix_timestamps) + 1)), counts)
 
+def filter_rm_section_time(row):
+    """Filter out the time it took to complete a given section from the internal
+    survey results for a given row."""
+    row_copy = row.copy()
+    for question in row.keys():
+        try:
+            if question + "Time" in row_copy:
+                row_copy.pop(question + "Time")
+            elif question[-4:] == "Time":
+                row_copy.pop(question)
+            elif "groupTime" in question:
+                row_copy.pop(question)
+        except KeyError:
+            continue
+    return row_copy
+
+def filter_rm_non_public(row):
+    """Filter out anyone who did not opt into the public release."""
+    if row["GeneralPrivacy"] == "Yes":
+        return row
+    else:
+        return False
+
+def filter_mod_ipaddrs(results):
+    """Exchange each ip address in the survey results data with a unique number."""
+    ipaddrs = {}
+    id_ = 0
+    for row in results:
+        if row["ipaddr"] in ipaddrs:
+            continue
+        else:
+            ipaddrs[row["ipaddr"]] = id_
+            id_ += 1
+    for row in results:
+        row["ipaddr"] = ipaddrs[row["ipaddr"]]
+    return results
+
+def filter_mod_datestamps(row):
+    """Remove the timestamp portion of the datestamps. THIS SHOULD ONLY BE USED
+    FOR PARTNER RELEASE, THE PUBLIC RELEASE SHOULD STRIP THESE ENTIRELY AND USE
+    filter_rm_datestamps."""
+    del(row["datestamp"])
+    row["submitdate"] = row["submitdate"].split(" ")[0]
+    row["startdate"] = row["startdate"].split(" ")[0]
+    return row
+
+def filter_rm_misc(row):
+    """Remove miscellaneous things such as row ID and lastpage."""
+    del(row["interviewtime"])
+    del(row["lastpage"])
+    del(row["refurl"])
+    del(row["id"])
+    return row
+
+def filter_rm_emailaddr(row):
+    """Remove the mailing list from the survey results. THIS IS PROBABLY THE 
+    BIGGEST POSSIBLE RELEASE SCREW UP, THESE MUST BE REMOVED FROM THE PUBLIC 
+    RELEASE."""
+    del(row["EmailAddress"])
+    return row
+
+def strip_data_for_partner_release(results):
+    """Strip the survey data so that it is suitable for release to trusted 
+    partners. This is almost the same thing as the public release, but includes
+    just a few more potentially identifying pieces of information such as the
+    date the survey was started and the date it was submitted, a unique number
+    representing an anonymized IP address, etc."""
+    stripped = []
+    results = filter_mod_ipaddrs(results)
+    for row in results:
+        if filter_rm_non_public(row):
+            stripped_row = filter_rm_section_time(row)
+            stripped_row = filter_mod_datestamps(stripped_row)
+            stripped_row = filter_rm_misc(stripped_row)
+            stripped_row = filter_rm_emailaddr(stripped_row)
+            stripped.append(stripped_row)
+        else:
+            continue
+    return stripped
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", help="Path to the survey results CSV file.")
     parser.add_argument("--structure",
                         help="Filepath to the TSV file describing survey structure.")
+    parser.add_argument("-o", "--output", help="Filepath to write output to.")
+    parser.add_argument("-p", "--public",
+                        help="Only output entries that have opted into the "
+                        "public dataset.")
+    parser.add_argument("--partner-release", dest="partner_release", type=int,
+                        help="Release a sample of size n appropriate for " +
+                        "release to trusted partners.")
     parser.add_argument("-b", "--brigade", action="store_true", help="Do brigading analysis.")
     parser.add_argument("--botcheck", action="store_true", help="Analyze possible bot replies.")
     parser.add_argument("--count-rise", dest="count_rise",
@@ -127,23 +251,50 @@ if __name__ == '__main__':
     results = []
 
     for row in inreader:
-        results.append(row)
+        if arguments.public:
+            if row["GeneralPrivacy"] == "Yes":
+                results.append(row)
+            else:
+                continue
+        else:
+            results.append(row)
     
     question_tallies = tally_answers(results, inreader.fieldnames)
         
     if arguments.brigade:
         brigade_checking.main()
+    elif arguments.partner_release:
+        if arguments.partner_release > len(results):
+            raise ValueError("Currently only" + str(len(results)) +
+                             " in dataset. Must be between this number and zero.")
+        sample = sample_results(results, arguments.partner_release)
+        stripped = strip_data_for_partner_release(sample)
+        if arguments.output:
+            outfile = open(arguments.output, mode="w", newline='')
+        else:
+            outfile = sys.stdout
+        fieldnames = []
+        for field in inreader.fieldnames:
+            if field in stripped[0]:
+                fieldnames.append(field)
+        outwriter = csv.DictWriter(outfile, fieldnames=fieldnames)
+        outwriter.writeheader()
+        for row in stripped:
+            outwriter.writerow(row)
+        quit()
     elif arguments.botcheck and arguments.structure:
         bot_checking.main()
     elif arguments.botcheck:
         raise ValueError("Argument --botcheck requires the survey structure to be "
-                         "passed along with it.")
+                         + "passed along with it.")
     elif arguments.count_rise:
         (x, y) = generate_x_y_count_rise(results, arguments.count_rise)
+        print(x, y)
         matplotlib.pyplot.xlabel("Hour since start of survey in multiples of six".title())
         matplotlib.pyplot.ylabel("Question response count".title())
         matplotlib.pyplot.scatter(x,y)
         matplotlib.pyplot.show()
+        quit()
     for question in inreader.fieldnames:
         print(question)
         for answer in question_tallies[question]:
