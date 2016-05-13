fbac6722f687939835008fbb587e531608ea0bba
John David Pressman <jdpostage@linuxmail.org>
2016-05-13 14:58:19 -0700

    Diff between the 2014 and 2016 survey results.

---
+++ 2016_2014_survey_diff.py
@@ -0,0 +1,29 @@
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile", help="The filepath to read columns from.")
+arguments = parser.parse_args()
+
+infile = open(arguments.infile)
+contents = infile.read()
+diffables = contents.split("----")
+
+def diff_diffable(diffable):
+    """Take in a diffable consisting of two columns of values from the 
+    2016 and 2014 surveys and figure out the difference between the two, returns
+    the printable representation of the diff."""
+    (d2016, d2014) = diffable.split("\n\n")
+    labels = [line.split(":")[0] for line in d2016.split("\n") if line]
+    d2016 = [float(line.split(":")[1].strip().split()[1]) 
+             for line in d2016.split("\n") if line]
+    d2014 = [float(line.split(":")[1].strip().split()[1][:-1]) / 100 
+             for line in d2014.split("\n") if line]
+    diff_nums = (np.array(d2016) - np.array(d2014)) * 100
+    conv = lambda s: "+" + s if float(s) > 0 else s
+    diff_num_strs = [conv(str(num)) for num in diff_nums]
+    outlines = [': '.join([str(value) for value in outline]) 
+                for outline in tuple(zip(labels, diff_num_strs))]
+    return '\n'.join(outlines)
+
+[print(diff_diffable(diffable), end="\n\n") for diffable in diffables]  
